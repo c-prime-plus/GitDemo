7c3f8bfa8959064f7ff40943563d5b6af385def0
HChao <1067680167@qq.com>
2019-04-24 14:13:46 +0800

    第一次修改

---
+++ HelloWeb/src/com/hc/test/Hello.java
@@ -0,0 +1,12 @@
+package com.hc.test;
+
+/**
+ * @Author: HChao
+ * @Description:
+ * @Date: 2019/4/24 14:10
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("HelloWorld");
+    }
+}
